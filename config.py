ecc3b2859e0e76e4d24b5bcd0675356d1a9453f4
Gulbendjkdsk <144287923+Gulbendjkdsk@users.noreply.github.com>
2024-11-30 20:02:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID", "7557261462:AAGFYjh3ZboHHoNbE5RIwD-1UrIx8Gsn54I"))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 LOG_GROUP = int(getenv("LOG_GROUP", ""))
